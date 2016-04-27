6b7ab3fe508252392bf5fbdebce3cfa7eb887d29
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-04-27 20:39:31 +0300

    Shop mechanics

--- ObjectPlacementUnityProj/Assets/Scripts/Core/App.cs
+++ ObjectPlacementUnityProj/Assets/Scripts/Core/App.cs
@@ -1,14 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class App
 {
-    private App() {}
+    public GameObject SelectedObject;
+    public GameObject SelectedPrefab;
+
     private static App _instance;
+    private ScenesManager _scenesManager;
+
     public static App Instance
     {
         get { return _instance ?? (_instance = new App()); }
     }
 
-    public GameObject SelectedObject;
-    public GameObject SelectedPrefab;
+    public ScenesManager ScenesManager
+    {
+        get { return _scenesManager ?? (_scenesManager = new ScenesManager()); }
+    }
+
+    private App()
+    {
+    }
+}
+
+public class ScenesManager
+{
+    public void OpenShop()
+    {
+        SceneManager.LoadScene("Shop",LoadSceneMode.Additive);
+    }
+
+    public void CloseShop()
+    {
+        SceneManager.UnloadScene("Shop");
+    }
 }
