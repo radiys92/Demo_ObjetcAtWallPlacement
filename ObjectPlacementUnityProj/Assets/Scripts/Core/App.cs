167f82762f228f80c1ec4e87b4720c8e59a200f0
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-04-27 18:34:10 +0300

    App & prefab selecting basics

---
+++ ObjectPlacementUnityProj/Assets/Scripts/Core/App.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class App
+{
+    private App() {}
+    private static App _instance;
+    public static App Instance
+    {
+        get { return _instance ?? (_instance = new App()); }
+    }
+
+    public GameObject SelectedObject;
+    public GameObject SelectedPrefab;
+}
