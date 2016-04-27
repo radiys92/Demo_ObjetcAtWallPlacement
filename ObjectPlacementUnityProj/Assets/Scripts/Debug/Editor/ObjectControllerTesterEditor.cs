3d091fbb84164b88070fa86349c10a2146ea925c
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-04-27 13:12:34 +0300

    Updated animations

--- ObjectPlacementUnityProj/Assets/Scripts/Debug/Editor/ObjectControllerTesterEditor.cs
+++ ObjectPlacementUnityProj/Assets/Scripts/Debug/Editor/ObjectControllerTesterEditor.cs
@@ -1,21 +1,35 @@
+using System;
+using System.Globalization;
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
+
+public enum States
+{
+    ABCDEF,
+    ACBDEF,
+    AdEXG,
+    bErDSa
+}
 
 [CustomEditor(typeof(ObjectControllerTester))]
 [CanEditMultipleObjects]
 public class ObjectControllerTesterEditor : Editor
 {
-	public override void OnInspectorGUI ()
-	{
-		if (!Application.isPlaying) {
-			EditorGUILayout.LabelField ("Can run only in play mode");
-			return;
-		}
-		
-		var t = target as ObjectControllerTester;
-		var con = t.GetComponent<IObjectController> ();
-		con.IsSelected = EditorGUILayout.Toggle ("Is selected", con.IsSelected);
-		con.IsMoving = EditorGUILayout.Toggle ("Is moving", con.IsMoving);
-		con.IsEditing = EditorGUILayout.Toggle ("Is editing", con.IsEditing);
-	}
+    States _selected;
+
+    public override void OnInspectorGUI()
+    {
+        if (!Application.isPlaying)
+        {
+            EditorGUILayout.LabelField("Can run only in play mode");
+            return;
+        }
+
+        var t = target as ObjectControllerTester;
+        var con = t.GetComponent<IObjectController>();
+        con.IsSelected = EditorGUILayout.Toggle("Is selected", con.IsSelected);
+        con.IsMoving = EditorGUILayout.Toggle("Is moving", con.IsMoving);
+        con.IsEditing = EditorGUILayout.Toggle("Is editing", con.IsEditing);
+    }
 }
